b9be89c2957a65b5b4a8f73807d62f215c543cd0
Cody Mercer <ushackerpros@gmail.com>
2014-12-30 19:38:54 -0600

    Update JavaFib-SourceCode.java

--- JavaFib-SourceCode.java
+++ JavaFib-SourceCode.java
@@ -1,12 +1,13 @@
 //Author:Cody Mercer
 //Java Fibonacci Program 
 
-import java.util.Scanner;
 /**
  * Java program to calculate and print Fibonacci number using both recursion and Iteration.
  * Fibonacci number is sum of previous two Fibonacci numbers fn= fn-1+ fn-2
  * first 10 Fibonacci numbers are 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
  */
+
+import java.util.Scanner;
 public class FibonacciCalculator {
     public static void main(String args[]) {
    
@@ -19,21 +20,12 @@ public class FibonacciCalculator {
             System.out.print(fibonacci2(i) +" ");
         }
     }
-    /*
-     * Java program for Fibonacci number using recursion.
-     * This program uses tail recursion to calculate Fibonacci number for a given number
-     * @return Fibonacci number
-     */
     public static int fibonacci(int number){
         if(number == 1 || number == 2){
             return 1;
         }
         return fibonacci(number-1) + fibonacci(number -2); //tail recursion
     }
-     /*
-     * Java program to calculate Fibonacci number using loop or Iteration.
-     * @return Fibonacci number
-     */
     public static int fibonacci2(int number){
         if(number == 1 || number == 2){
             return 1;
